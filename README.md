032227fa246b134a47bdde60e0fa717b9f63bdc8
Jason Rundell <jason.rundell@gmail.com>
2020-12-08 00:36:25 -0500

    Small updates to app

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # amplify-monorepo-test
-
-<!-- 1 -->
--- app-a/src/App.js
+++ app-a/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>APP A</p>
+        <p>React APP A</p>
         <a
           className="App-link"
           href="https://reactjs.org"
