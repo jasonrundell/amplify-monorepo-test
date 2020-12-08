00a5fcdae927ece9188667d80a90b3930561a674
Jason Rundell <jason.rundell@gmail.com>
2020-12-08 00:39:43 -0500

    Small updates again

--- app-a/src/App.js
+++ app-a/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>React APP A</p>
+        <p>React APP A - Hello World</p>
         <a
           className="App-link"
           href="https://reactjs.org"
--- app-b/src/App.js
+++ app-b/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>React APP B</p>
+        <p>React APP B - Hello World</p>
         <a
           className="App-link"
           href="https://reactjs.org"
